d308353ac5cd180b29ba89dcd390cb3a4d17c90e
Павел Семенюк <tellermcs@gmail.com>
2023-01-08 14:09:23 +0200

    migrate to .net 7

--- Verbum.Application/Common/Behaviors/ValidationBehavior.cs
+++ Verbum.Application/Common/Behaviors/ValidationBehavior.cs
@@ -11,7 +11,7 @@ namespace Verbum.Application.Common.Behaviors
         public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
 
         public Task<TResponse> Handle(TRequest request,
-            CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+             RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var context = new ValidationContext<TRequest>(request);
             var failtures = _validators
